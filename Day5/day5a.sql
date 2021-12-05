8f4558ff9a6b8aa1232d1137d00cc8c720df149c
saxus <saxus@muportal.hu>
2021-12-05 07:55:18 +0100

    Day 5

---
+++ Day5/day5a.sql
@@ -0,0 +1,28 @@
+﻿with
+    droutes (x1, y1, x2, y2, dist, dx, dy) as (
+	select origin[0]::integer, origin[1]::integer,
+	       target[0]::integer, target[1]::integer,
+	       greatest(
+	           abs(target[0]::integer - origin[0]::integer),
+	           abs(target[1]::integer - origin[1]::integer)
+	       ),
+	       sign(target[0] - origin[0]),
+	       sign(target[1] - origin[1])
+        from input5 
+        where direction in ('horiz', 'vert')
+    ),
+    allpoints (x, y) as (
+	select x1 + dx * t AS x, y1 + dy * t AS y 
+        from droutes, unnest((select array_agg(t) from generate_series(0, dist) t)) ts(t)
+    ),
+    overlps (x, y, count) as (    
+	select x, y, count(*)
+	from allpoints
+	group by x, y
+    )
+select count(*)
+from overlps 
+where count > 1
+
+    
+
---
+++ Day5/day5input.sql
@@ -0,0 +1,43 @@
+﻿do $$
+declare 
+  input text;
+  r record;
+begin
+	drop table if exists input5;
+
+	create table if not exists input5 (
+		origin point,
+		target point,
+		direction varchar
+	);
+
+	input := 
+'0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2';
+
+	input := replace(input, E'\r', '');
+
+	insert into input5
+	with raw (rec) as (
+		select string_to_array(a, ' -> ') rec from unnest(string_to_array(input, E'\n')) a
+	),
+	points (origin, target) as (
+		select rec[1]::point, rec[2]::point from raw
+	)
+	select
+		origin, target, 
+		case when origin[0] = target[0] then 'horiz'
+		     when origin[1] = target[1] then 'vert'
+                     else 'diag' end direction
+        from points;
+end $$;
+
+select * from input5;
